f85d7c4225f1a8b7b18704359b06fbaadbb0606d
Ondrej Perutka <ondrej.perutka@angelcam.com>
2017-01-13 11:24:53 +0100

    Fix typo in stack formatting function

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tbsignal",
-    version='0.1.0',
+    version='0.1.1',
     description="Angelcam traceback signal handler",
     keywords="traceback signal",
     author="Angelcam",
--- tbsignal/tbsignal.py
+++ tbsignal/tbsignal.py
@@ -3,7 +3,7 @@ import sys, traceback, signal
 def traceback_signal_handler(sig, frame):
     message = "Signal %d received.\n" % sig
     message += "Traceback (most recent call last):\n"
-    message += ''.join(traceback.from_stack(frame))
+    message += ''.join(traceback.format_stack(frame))
     print(message, file=sys.stderr)
 
 def register_handler(sig=signal.SIGUSR1):
